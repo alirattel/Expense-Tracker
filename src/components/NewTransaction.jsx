9c89b22e4556f9c61dad4160366c4fd28758abb7
alirattel <105751548+alirattel@users.noreply.github.com>
2022-07-17 17:54:30 +0300

    Update NewTransaction.jsx

--- src/components/NewTransaction.jsx
+++ src/components/NewTransaction.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Typography, Box, TextField, Button, styled } from '@mui/material';
+import Swal from 'sweetalert2'
 
 const Container = styled(Box)`
     display: flex;
@@ -21,21 +22,34 @@ const StyledButton = styled(Button)`
 const NewTransaction = ({ addTransaction }) => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState();
-
     const newTransaction = e => {
         const transaction = {
             id: Math.floor(Math.random() * 100000000),
             text: text,
             amount: +amount
         }
-        addTransaction(transaction);
+        if (text === "" || text.match(/^\s+$/)) {
+            Swal.fire({icon:'error',text:'يرجى ملئ هذا الحقل'})
+            document.getElementById("text").style.backgroundColor = 'rgba(255, 0, 0, 0.1)';
+        }
+        else if (amount === '' || isNaN(amount) || amount.match(/^\s+$/)) {
+            Swal.fire({icon:'error',text:'يرجى إدخال رقم '})
+            document.getElementById("am").style.backgroundColor = 'rgba(255, 0, 0, 0.1)';
+        }
+        else {
+            addTransaction(transaction);
+            document.getElementById("am").style.backgroundColor = '#fff';
+            document.getElementById("text").style.backgroundColor = '#fff';
+            document.querySelector('.am').value = ''
+            document.querySelector('.text').value = '';
+        }
     }
-    
+
     return (
         <Container>
-            <Typography style={{ fontFamily:'Kufam, sans-serif'}} variant="h5">معاملة جديدة</Typography>
-            <TextField style={{ fontFamily:'Kufam, sans-serif'}} value={text} label="...الراتب , بضائع , ضرائب الخ " onChange = {(e) => setText(e.target.value)} />
-            <TextField style={{ fontFamily:'Kufam, sans-serif'}} value={amount} label="..المبلغ" onChange = {(e) => setAmount(e.target.value)}  />
+            <Typography style={{ fontFamily: 'Kufam, sans-serif' }} variant="h5">معاملة جديدة</Typography>
+            <TextField className='text' id='text' style={{ fontFamily: 'Kufam, sans-serif' }} value={text} label="...الراتب , بضائع , ضرائب الخ " onChange={(e) => setText(e.target.value)} requierd />
+            <TextField className='am' id='am' style={{ fontFamily: 'Kufam, sans-serif' }} value={amount} label="..المبلغ" onChange={(e) => setAmount(e.target.value)} requierd />
             <StyledButton variant="contained" onClick={newTransaction}>إدخال المعاملة</StyledButton>
         </Container>
     )
